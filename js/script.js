a1ab778ee5b1855a8b05036977bff90c4d15c151
Caterina Bertotti <bertotti4997@gmail.com>
2021-03-17 17:07:55 +0100

    Creato ciclo for in e stampato il contenuto dell'oggetto

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ var studente = {
 console.log(studente);
 
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+for ( var key in studente ) {
+    console.log(key + ': ' + studente[key]);
+}
+
 // Creare un array di oggetti di studenti.
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
