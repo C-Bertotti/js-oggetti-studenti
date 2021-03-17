9c90c2fac21094b3c2832073737e1fc52b01336e
Caterina Bertotti <bertotti4997@gmail.com>
2021-03-17 17:48:06 +0100

    Fix ;

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ var studente = {
     nome: 'Caterina',
     cognome: 'Bertotti',
     eta: 23
-}
+};
 
 console.log(studente);
 
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 for ( var key in studente ) {
     console.log(key + ': ' + studente[key]);
-}
+};
 
 // Creare un array di oggetti di studenti.
 var studenti = [
@@ -32,7 +32,7 @@ var studenti = [
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 for ( var i = 0; i < studenti.length; i++ ) {
     console.log(studenti[i]['nome'] + ' ' + studenti[i]['cognome']);
-}
+};
 
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 var nuoviStudenti = [];
@@ -54,4 +54,4 @@ while ( oneMore !== 'stop' ) {
     nuoviStudenti.push(nuovoStudente);
 
     var oneMore = prompt('Vuoi inserire un nuovo studente? Se no, scrivi stop, altrimenti fai click su un qualsiasi tasto e poi premi invio');
-}
+};
